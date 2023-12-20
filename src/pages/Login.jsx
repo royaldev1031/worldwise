bf381d9078eb55001a8919650e9533d07dbc1a21
Smart Dev <48024874+smartdev0612@users.noreply.github.com>
2023-12-20 04:50:46 -0500

    add logo and main pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,16 @@
-import styles from "./Login.module.css";
+import { useState } from 'react'
+import styles from './Login.module.css'
+import PageNav from '../components/PageNav'
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [email, setEmail] = useState('jack@example.com')
+  const [password, setPassword] = useState('qwerty')
 
   return (
     <main className={styles.login}>
+      <PageNav />
+
       <form className={styles.form}>
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
@@ -33,5 +37,5 @@ export default function Login() {
         </div>
       </form>
     </main>
-  );
+  )
 }
