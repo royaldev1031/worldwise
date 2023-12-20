59ec81a6fb5eb612f177d00f9362e32f304a154a
Smart Dev <48024874+smartdev0612@users.noreply.github.com>
2023-12-20 05:24:41 -0500

    add nested route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router-dom'
 import Logo from './Logo'
 import styles from './Sidebar.module.css'
 
@@ -6,7 +7,8 @@ function Sidebar() {
     <div className={styles.sidebar}>
       <Logo />
 
-      <p>List of cities</p>
+      <Outlet />
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
