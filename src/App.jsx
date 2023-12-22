42ce6449fc4d14c7799450cb2001515a14e0d880
Smart Dev <48024874+smartdev0612@users.noreply.github.com>
2023-12-22 04:36:56 -0500

    create country list page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AppLayout from './pages/AppLayout'
 import PageNotFound from './pages/PageNotFound'
 import Login from './pages/Login'
 import CityList from './components/CityList'
+import CountryList from './components/CountryList'
 
 const BASE_URL = 'http://localhost:8000'
 
@@ -47,7 +48,10 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>List of countries</p>} />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
